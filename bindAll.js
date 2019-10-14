1d29f8ab198dfe3606ed5434bfc7260f29db112f
Matthew Smith <niftinessafoot@gmail.com>
2019-10-14 11:46:14 -0400

    Minifying mangles the name pre-binding. Moving to string-based iteration.

--- bindAll.js
+++ bindAll.js
@@ -3,12 +3,13 @@
  * Useful for passing methods through React components, etc.
  *
  * @param  {object}    target  Usually `this` in the constructor of a class.
- * @param  {...[array|string]} methods The methods to be bound. Can be a running list of arguments or an array. example: this.methodOne, [this.methodTwo, this.methodThree]
+ * @param  {...[array|string]} methods The methods to be bound. Can be a running list of arguments or an array. example: 'methodOne', ['methodTwo', 'methodThree']
  */
 export default function(target, ...methods){
   [].concat(...methods).forEach(ele => {
-    if(typeof(ele) === 'function'){
-      target[ele.name] = ele.bind(target);
+    const fn = target[ele];
+    if(fn && typeof(fn) === 'function'){
+      target[ele] = fn.bind(target);
     }
-  })
+  });
 }
